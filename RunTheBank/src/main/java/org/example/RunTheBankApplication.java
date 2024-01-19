bb7bd08d376c65f0b3713f3ee57741a467f5931c
ferdemello <110197887+fermelloG3@users.noreply.github.com>
2024-01-19 06:14:59 -0300

    Configure H2 Database

--- RunTheBank/src/main/java/org/example/RunTheBankApplication.java
+++ RunTheBank/src/main/java/org/example/RunTheBankApplication.java
@@ -1,2 +1,12 @@
-package org.example;public class RunTheBankApplication {
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunTheBankApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(RunTheBankApplication.class, args);
+    }
 }
