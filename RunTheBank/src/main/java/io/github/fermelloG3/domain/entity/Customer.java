0042b0c9d15f3ddc0d381135bd31c6de836ec845
ferdemello <110197887+fermelloG3@users.noreply.github.com>
2024-01-19 07:44:15 -0300

    Create Customer Entity

---
+++ RunTheBank/src/main/java/io/github/fermelloG3/domain/entity/Customer.java
@@ -0,0 +1,60 @@
+package io.github.fermelloG3.domain.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String document;
+
+    private String adress;
+
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
