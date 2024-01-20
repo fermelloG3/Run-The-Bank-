746489d0f4482f4bdfa24f18743a7e76274dcb36
ferdemello <110197887+fermelloG3@users.noreply.github.com>
2024-01-20 03:38:30 -0300

    Create Payment Model and enum pachage

---
+++ RunTheBank/src/main/java/io/github/fermelloG3/domain/entity/Payment.java
@@ -0,0 +1,70 @@
+package io.github.fermelloG3.domain.entity;
+
+import io.github.fermelloG3.domain.enums.PaymentStatus;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private BigDecimal amount;
+
+    @Column
+    private Long senderAccountId;
+
+    @Column
+    private Long receiverAccountId;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private PaymentStatus status;
+
+    public Payment(){
+        
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Long getSenderAccountId() {
+        return senderAccountId;
+    }
+
+    public void setSenderAccountId(Long senderAccountId) {
+        this.senderAccountId = senderAccountId;
+    }
+
+    public Long getReceiverAccountId() {
+        return receiverAccountId;
+    }
+
+    public void setReceiverAccountId(Long receiverAccountId) {
+        this.receiverAccountId = receiverAccountId;
+    }
+
+    public PaymentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PaymentStatus status) {
+        this.status = status;
+    }
+}
---
+++ RunTheBank/src/main/java/io/github/fermelloG3/domain/enums/PaymentStatus.java
@@ -0,0 +1,7 @@
+package io.github.fermelloG3.domain.enums;
+
+public enum PaymentStatus {
+    PENDING,
+    COMPLETED,
+    CANCELLED,
+}
