a71cf1e94ccaea9fe6990147063e9f7d633c855d
ferdemello <110197887+fermelloG3@users.noreply.github.com>
2024-01-21 06:01:53 -0300

    Create CustomerDTO

---
+++ RunTheBank/src/main/java/io/github/fermelloG3/rest/dto/CustomerDTO.java
@@ -0,0 +1,43 @@
+package io.github.fermelloG3.rest.dto;
+
+public class CustomerDTO {
+
+    private Long id;
+    private String name;
+    private String document;
+    private String address;
+
+    public CustomerDTO(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
