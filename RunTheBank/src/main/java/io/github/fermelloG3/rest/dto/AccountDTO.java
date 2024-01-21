b96efebf75d85c259be957ce2f29d66802c312fd
ferdemello <110197887+fermelloG3@users.noreply.github.com>
2024-01-21 06:04:11 -0300

    Create AccountDTO

---
+++ RunTheBank/src/main/java/io/github/fermelloG3/rest/dto/AccountDTO.java
@@ -0,0 +1,45 @@
+package io.github.fermelloG3.rest.dto;
+
+import java.math.BigDecimal;
+
+public class AccountDTO {
+
+    private Long id;
+    private String agency;
+    private BigDecimal balance;
+    private Boolean status;
+
+    public AccountDTO(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+}
